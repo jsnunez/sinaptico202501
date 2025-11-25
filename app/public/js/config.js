d79b7ad58a2f76d126a4f188d9ef1803845a9263
cpcorientedeveloper <jsnunez94@gmail.com>
2025-11-25 11:59:22 -0500

    seleccionar windows o linux

--- app/public/js/config.js
+++ app/public/js/config.js
@@ -1,2 +1,5 @@
-// const API_BASE_URL = "http://localhost:4000";
-const API_BASE_URL = "https://sinaptic11.cpcoriente.org";
+// Detect operating system and set appropriate API URL
+const isWindows = navigator.platform.toLowerCase().includes('win');
+const API_BASE_URL = isWindows 
+    ? "http://localhost:4000"  // Windows
+    : "https://sinaptic11.cpcoriente.org";  // Linux
