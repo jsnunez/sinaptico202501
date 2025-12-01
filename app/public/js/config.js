71ca0f5ed487811c37428a4592d7dea968a0cdb6
cpcorientedeveloper <jsnunez94@gmail.com>
2025-12-01 09:13:11 -0500

    produccion

--- app/public/js/config.js
+++ app/public/js/config.js
@@ -1,5 +1,5 @@
 // Detect operating system and set appropriate API URL
 const isWindows = navigator.platform.toLowerCase().includes('win');
-const API_BASE_URL = isWindows 
-    ? "http://localhost:4000"  // Windows
-    : "https://sinaptic11.cpcoriente.org";  // Linux
+// const API_BASE_URL =  "http://localhost:4000" 
+
+ const API_BASE_URL =  "https://sinaptic11.cpcoriente.org";  // Linux
